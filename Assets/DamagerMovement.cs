c1dd26d52e507d8c88afc1e4fc1aa851ebcdd7dc
Sartharis <jan.orlowsk@gmail.com>
2019-10-05 20:41:40 -0400

    Alll my stuff

--- Assets/DamagerMovement.cs
+++ Assets/DamagerMovement.cs
@@ -10,11 +10,15 @@ public class DamagerMovement : MonoBehaviour
     [SerializeField] float randomForce = 0.0f;
     [SerializeField] float randomTimeMin = 0.0f;
     [SerializeField] float randomTimeMax = 0.0f;
+    [SerializeField] bool rotateVel = false;
+    private float targetScale = 1.0f;
+    private Vector3 startScale;
     private Rigidbody2D body;
 
     // Start is called before the first frame update
     void Start()
     {
+        startScale = transform.localScale;
         body = GetComponent<Rigidbody2D>();    
         StartCoroutine(RandomMove());
     }
@@ -26,8 +30,12 @@ public class DamagerMovement : MonoBehaviour
             if(GameObject.FindGameObjectWithTag("Player") && (GameObject.FindGameObjectWithTag("Player").transform.position - transform.position).magnitude < 15.0f)
             {
                 yield return new WaitForSeconds(Random.Range(randomTimeMin, randomTimeMax));
+                targetScale = 1.2f;
+                yield return new WaitForSeconds(0.4f);
                 body.AddForce(Random.insideUnitCircle * randomForce);
                 body.AddForce(followForce * (GameObject.FindGameObjectWithTag("Player").transform.position - transform.position).normalized);
+                targetScale = 1.0f;
+                //transform.localScale = startScale * 0.7f;
             }
             yield return new WaitForEndOfFrame();
         }
@@ -35,6 +43,8 @@ public class DamagerMovement : MonoBehaviour
 
     private void Update()
     {
+        transform.localScale = Vector3.Lerp(transform.localScale, startScale * targetScale, 0.1f);
+
         lifeTime -= Time.deltaTime;
         if (lifeTime < 0)
         {
@@ -45,7 +55,7 @@ public class DamagerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(body.velocity.magnitude > 0.01f)
+        if(body.velocity.magnitude > 0.01f && rotateVel)
         {
             body.SetRotation(Quaternion.FromToRotation(new Vector3(1,0,0), body.velocity.normalized));
         }
